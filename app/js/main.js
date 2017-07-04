1c6e3ac20e3611ffb3830d55175b0a3944284be6
qiuxjgit <265843468@qq.com>
2017-07-04 21:52:48 +0800

    first commit

---
+++ app/js/main.js
@@ -0,0 +1,7 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var QuestionApp = require('./components/QuestionApp.js')
+var mainCom = ReactDOM.render(
+	<QuestionApp />,
+	document.getElementById('app')
+	)
